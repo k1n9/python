5c8043c31889172ffa280ea3f20560e1c564b91f
king <k1n9@users.noreply.github.com>
2016-01-30 14:40:25 +0800

    Update main.py
    添加了一个简单正则，让输出结果更加符合需要
    后面还会继续添加一些

--- code_audit/main.py
+++ code_audit/main.py
@@ -3,8 +3,9 @@
 @author: K1n9
 '''
 import os
-import cgi
 import sys
+import cgi
+import re
 
 def f_list(rootdir):
     files = set()
@@ -20,7 +21,7 @@ def f_list(rootdir):
     return files_list
 
 
-def f_search(files_list,str_list):
+def f_search(files_list,str_list,flag):
     lines_dic = {}
 
     for filename in files_list:
@@ -34,7 +35,10 @@ def f_search(files_list,str_list):
                 cont = f2.readline()
                 while cont != '':
                     if val in cont.lower():
-                        lines_cont.add(cont.strip('\n').strip('\t'))
+                        if flag and re.search(val+'.*\$',cont.lower()) != None:
+                            lines_cont.add(cont.strip('\n').strip('\t'))
+                        elif not flag:
+                            lines_cont.add(cont.strip('\n').strip('\t'))
                     cont = f2.readline()
                 f2.close()
         if len(lines_cont) != 0:
@@ -44,7 +48,7 @@ def f_search(files_list,str_list):
 
 
 def output_html_all(output_list):
-    fout = open('all_output.html','w')
+    fout = open(sys.argv[1]+'.html','w')
     fout.write('<html>')
     fout.write('<body>')
     fout.write('<h4>parameter:</h4>')
@@ -61,19 +65,21 @@ def output_html_all(output_list):
     output_table(fout,output_list[5])
     fout.write('</body>')
     fout.write('</html>')
-    print 'Output finished'
+    print 'Output finished!'
+    print sys.argv[1]+'.html'
     fout.close()
 
 
 def output_html_one(output_list):
-    fout = open('one_output.html','w')
+    fout = open(sys.argv[2]+'.html','w')
     fout.write('<html>')
     fout.write('<body>')
     fout.write('<h4>%s:</h4>' % sys.argv[2])
     output_table(fout,output_list[0])
     fout.write('</body>')
     fout.write('</html>')
-    print 'Output finished'
+    print 'Output finished!'
+    print sys.argv[2]+'.html'
     fout.close()
 
 
@@ -94,24 +100,31 @@ def output_table(fout,output):
 
 
 if __name__ == "__main__":
-    parameter  = ['$_get','$_post','$_cookie','$_request','$_files']
-    sql = ['sql','select','insert','update','delete','from','where','limit']
-    include = ['include','require','include_once','require_once']
-    execute = ['eval','assert']
-    output = ['echo','print']
-    files = ['file_get_contents','file_put_contents']
-    keyword_list = [parameter,sql,include,execute,output,files]
-    
-    rootdir = sys.argv[1]
-  
-    files_list = f_list(rootdir)
+    if len(sys.argv) == 1:
+        print 'Usage1: ' + sys.argv[0] + ' /home/code_dir'
+        print 'Usage2: ' + sys.argv[0] + ' /home/code_dir ' + 'keyword'
+        exit()
 
+    rootdir = sys.argv[1]
+    files_list = f_list(rootdir)
     output_list = []
+
     if len(sys.argv) == 2:
+        parameter  = ['$_get','$_post','$_cookie','$_request','$_files']
+        sql = ['sql','select','insert','update','delete','from','where','limit']
+        include = ['include','require','include_once','require_once']
+        execute = ['eval','assert']
+        output = ['echo','print']
+        files = ['file_get_contents','file_put_contents']
+        keyword_list = [parameter,sql,include,execute,output,files]
+
         for val in keyword_list:
-            output_list.append(f_search(files_list,val))
+            flag = 1
+            if '$_get' in val:
+                flag = 0
+            output_list.append(f_search(files_list,val,flag))
         output_html_all(output_list)
     else:
         keyword = [sys.argv[2]]
-        output_list.append(f_search(files_list,keyword))
+        output_list.append(f_search(files_list,keyword,0))
         output_html_one(output_list)
